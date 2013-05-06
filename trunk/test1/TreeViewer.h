965a93813a72d6078fd8c1d333dab9a4ce4851ab
ESavinkhin@gmail.com <ESavinkhin@gmail.com@22e8ef50-b267-d218-9388-f15fc8796bed>
2013-05-06 19:18:03 +0000

    поменял алгортим отображения дерева

--- trunk/test1/TreeViewer.h
+++ trunk/test1/TreeViewer.h
@@ -14,31 +14,36 @@ using namespace System;
 	  void Build(Graphics^ g) {
 		 g->Clear(SystemColors::Control);
 		 SizeF f = g->VisibleClipBounds.Size;
-		 this->traverse(m_tree->Top, f.Width / 2, 10, g);
+		 k = m_tree->Depth;
+		 this->traverse(m_tree->Top, f.Width / 2, 10, g, 1);
 	  }
   private:
 	 static int s_size_font = 10;
 	 Tree^ m_tree;
-	 void traverse(Node^ node, int x, int y, Graphics^ g) {
+	 int k;
+	 void traverse(Node^ node, int x, int y, Graphics^ g, int level) {		
 		if (node == nullptr) 
 		{
 			return;
 		}
 	 	if (node->Left != nullptr) 
-		{
-			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, x - 40 + 10, y + 40 + 10);
-			traverse(node->Left,x-40, y + 40, g);
+		{			
+			int nx = x - 100 * (k - 2) / (level * level);
+			int ny = y + 40;
+			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, nx + 10, ny + 10);
+			traverse(node->Left, nx, ny , g, level + 1);			
 		}
 		if (node->Right != nullptr) 
-		{
-			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, x + 40 + 10, y + 40 + 10);
-			traverse(node->Right, x + 40, y + 40, g);
+		{	
+			int nx = x + 100 * (k - 2) / (level * level);
+			int ny = y + 40;
+			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, nx + 10, ny + 10);
+			traverse(node->Right, nx, ny , g, level + 1);						
 		}
-		Rectangle rect = Rectangle(x,y,30,30);
+		Rectangle rect = Rectangle(x, y, 30, 30);
 		SolidBrush^ redBrush = gcnew SolidBrush( node->Last ?Color::Green:Color::Red  );
 		g->FillEllipse(redBrush, rect);		
 		g->DrawString( Convert::ToString(node->Value), gcnew System::Drawing::Font( "Arial", s_size_font ), gcnew SolidBrush( Color::Blue ), x + 12 - Convert::ToString(node->Value)->Length * 2.5, y + 7 );
-		
 	}
  };
 }
