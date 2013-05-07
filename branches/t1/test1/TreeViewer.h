cd3e25c721793144a2cbf469249c884b1e7594bb
ESavinkhin@gmail.com <ESavinkhin@gmail.com@22e8ef50-b267-d218-9388-f15fc8796bed>
2013-05-07 19:11:04 +0000

    try add scroll

--- branches/t1/test1/TreeViewer.h
+++ branches/t1/test1/TreeViewer.h
@@ -3,23 +3,33 @@
 namespace test1 {
 using namespace System::Drawing;
 using namespace System;
+using namespace System::Collections;
  public ref class TreeViewer
  {
   public:
 	 TreeViewer(Tree^ tree)
 	  {
 		 this->m_tree = tree;
+		 this->m_list = gcnew ArrayList();
+		 m_list->Add(550);
+		 m_list->Add(290);
+		 m_list->Add(140);
+		 m_list->Add(70);
+		 m_list->Add(30);
+		 m_list->Add(20);
+		 m_list->Add(15);
 	  }
 
 	  void Build(Graphics^ g) {
 		 g->Clear(SystemColors::Control);
 		 SizeF f = g->VisibleClipBounds.Size;
 		 k = m_tree->Depth;
-		 this->traverse(m_tree->Top, f.Width / 2, 10, g, 1);
+		 this->traverse(m_tree->Top, 400 / 2, 10, g, 1);
 	  }
   private:
 	 static int s_size_font = 10;
 	 Tree^ m_tree;
+	 IList^ m_list;
 	 int k;
 	 void traverse(Node^ node, int x, int y, Graphics^ g, int level) {		
 		if (node == nullptr) 
@@ -28,15 +38,15 @@ using namespace System;
 		}
 	 	if (node->Left != nullptr) 
 		{			
-			int nx = x - 100 * (k - 2) / (level * level);
-			int ny = y + 40;
+			int nx = x - (int) m_list[level - 1];
+			int ny = y + 60;
 			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, nx + 10, ny + 10);
 			traverse(node->Left, nx, ny , g, level + 1);			
 		}
 		if (node->Right != nullptr) 
-		{	
-			int nx = x + 100 * (k - 2) / (level * level);
-			int ny = y + 40;
+		{				
+			int nx = x + (int) m_list[level - 1];
+			int ny = y + 60;
 			g->DrawLine(gcnew Pen(Color::Green), x + 10, y + 10, nx + 10, ny + 10);
 			traverse(node->Right, nx, ny , g, level + 1);						
 		}
